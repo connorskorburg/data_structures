47664976c3b16395956ff07066317edb54ed126f
Connor <connorskorburg@gmail.com>
2020-12-09 20:01:50 -0600

    added node class to sll for python

--- Python/SinglyLinkedList/singly_linked_list.py
+++ Python/SinglyLinkedList/singly_linked_list.py
@@ -1,5 +1,10 @@
 # create linked list with no built-in methods
 
+class Node:
+  def __init__(self, value, next):
+    self.value = value
+    self.next = next
+
 class SinglyLinkedList:
   def __init__(self):
     pass
